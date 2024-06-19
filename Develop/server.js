0810a7ed68c77a827526ac8c20a40e618ac9c746
dameine yang <dameiney123@gmail.com>
2024-06-19 17:27:39 -0500

    server.js working

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,33 @@
 const inquirer = require('inquirer');
-const {Pool} = require('pg');
+const pg = require('pg');
+const { Pool } = pg;
 
 const PORT = process.env.PORT || 3001;
+let connected = false;  // Variable to track database connection
 
 // Connect to PG
 const pool = new Pool({
     user: 'postgres',
-    password: 'Daedae0420',
+    password: 'password',
     host: 'localhost',
     database: 'employee_db',
+    port: 5432
 });
 
-pool.connect();
+pool.connect((err) => {
+    if (err) {
+        console.log('Error connecting to the database');
+        process.exit(1);
+    } else {
+        connected = true;  // Set the connection flag to true
+        console.log('Connected to the database');
+        menu();  // Start the menu only after successful connection
+    }
+});
 
 // Start server
 const menu = () => {
+    if (!connected) return;  // Prevent menu from running if not connected
     inquirer.prompt({
         type: 'list',
         name: 'choice',
@@ -29,7 +42,7 @@ const menu = () => {
             'Update an employee role',
             'Exit'
         ]
-    }).then(({choice}) => {
+    }).then(({ choice }) => {
         switch (choice) {
             case 'View all departments':
                 viewDepartments();
@@ -40,21 +53,20 @@ const menu = () => {
             case 'View all employees':
                 viewEmployees();
                 break;
-            case 'Add an department':
+            case 'Add a department':
                 addDepartment();
                 break;
             case 'Add a role':
                 addRole();
                 break;
-            case 'Add a employee':
+            case 'Add an employee':
                 addEmployee();
                 break;
             case 'Update an employee role':
                 updateEmployeeRole();
                 break;
-            case 'Exit':
-                pool.end();
-                break;
+            default:
+                process.exit();
         }
     });
 };
@@ -62,9 +74,13 @@ const menu = () => {
 // View all departments
 const viewDepartments = () => {
     pool.query('SELECT * FROM department', (err, res) => {
-        if (err) throw err;
-        console.table(res.rows);
-        menu();
+        if (err) {
+            console.log("Error fetching department", err);
+            return;
+        } else {
+            console.table(res.rows);
+            menu();
+        }
     });
 };
 
@@ -74,11 +90,15 @@ const viewRoles = () => {
     SELECT employee_role.role_id, employee_role.title, department.department_name AS department, employee_role.salary 
     FROM employee_role 
     JOIN department ON employee_role.department_id = department.department_id`;
-  pool.query(query, (err, res) => {
-    if (err) throw err;
-    console.table(res.rows);
-    menu();
-  });
+    pool.query(query, (err, res) => {
+        if (err) {
+            console.log("Error fetching roles", err);
+            return;
+        } else {
+            console.table(res.rows);
+            menu();
+        }
+    });
 };
 
 // View all employees
@@ -89,11 +109,15 @@ const viewEmployees = () => {
     JOIN employee_role ON employee.role_id = employee_role.role_id
     JOIN department ON employee_role.department_id = department.department_id
     LEFT JOIN employee manager ON employee.manager_id = manager.employee_id`;
-  pool.query(query, (err, res) => {
-    if (err) throw err;
-    console.table(res.rows);
-    menu();
-  });
+    pool.query(query, (err, res) => {
+        if (err) {
+            console.log("Error fetching employees", err);
+            return;
+        } else {
+            console.table(res.rows);
+            menu();
+        }
+    });
 };
 
 // Add a department
@@ -102,10 +126,13 @@ const addDepartment = () => {
         type: 'input',
         name: 'department',
         message: 'Enter the name of the department'
-    }).then(({department}) => {
+    }).then(({ department }) => {
         pool.query('INSERT INTO department (department_name) VALUES ($1)', [department], (err, res) => {
-            if (err) throw err;
-            console.log('Department added');
+            if (err) {
+                console.error('Error adding department', err);
+            } else {
+                console.log('Department added');
+            }
             menu();
         });
     });
@@ -114,90 +141,53 @@ const addDepartment = () => {
 // Add a role
 const addRole = () => {
     pool.query('SELECT * FROM department', (err, res) => {
-        if (err) throw err;
+        if (err) {
+            console.error('Error fetching departments', err);
+            menu();
+        } else {
+            const departments = res.rows.map(department => ({
+                name: department.department_name,
+                value: department.department_id
+            }));
 
-        const departments = res.rows.map(department => ({
-            name: department.department_name,
-            value: department.department_id
-        }));
-
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'title',
-                message: 'Enter the title of the role'
-            },
-            {
-                type: 'input',
-                name: 'salary',
-                message: 'Enter the salary of the role'
-            },
-            {
-                type: 'list',
-                name: 'department',
-                message: 'Select the department of the role',
-                choices: departments
-            }
-        ]).then(({title, salary, department}) => {
-            pool.query('INSERT INTO employee_role (title, salary, department_id) VALUES ($1, $2, $3)', [title, salary, department], (err, res) => {
-                if (err) throw err;
-                console.log('Role added');
-                menu();
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: 'Enter the title of the role'
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: 'Enter the salary of the role'
+                },
+                {
+                    type: 'list',
+                    name: 'department',
+                    message: 'Select the department of the role',
+                    choices: departments
+                }
+            ]).then(({ title, salary, department }) => {
+                pool.query('INSERT INTO employee_role (title, salary, department_id) VALUES ($1, $2, $3)', [title, salary, department], (err, res) => {
+                    if (err) {
+                        console.error('Error adding role', err);
+                    } else {
+                        console.log('Role added');
+                    }
+                    menu();
+                });
             });
-        });
+        }
     });
 };
 
 // Add an employee
 const addEmployee = () => {
     pool.query('SELECT * FROM employee_role', (err, res) => {
-        if (err) throw err;
-
-        const roles = res.rows.map(role => ({
-            name: role.title,
-            value: role.role_id
-        }));
-
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'first_name',
-                message: 'Enter the first name of the employee'
-            },
-            {
-                type: 'input',
-                name: 'last_name',
-                message: 'Enter the last name of the employee'
-            },
-            {
-                type: 'list',
-                name: 'role',
-                message: 'Select the role of the employee',
-                choices: roles
-            }
-        ]).then(({first_name, last_name, role}) => {
-            pool.query('INSERT INTO employee (first_name, last_name, role_id) VALUES ($1, $2, $3)', [first_name, last_name, role], (err, res) => {
-                if (err) throw err;
-                console.log('Employee added');
-                menu();
-            });
-        });
-    });
-};
-
-// Update an employee role
-const updateEmployeeRole = () => {
-    pool.query('SELECT * FROM employee', (err, res) => {
-        if (err) throw err;
-
-        const employees = res.rows.map(employee => ({
-            name: `${employee.first_name} ${employee.last_name}`,
-            value: employee.employee_id
-        }));
-
-        pool.query('SELECT * FROM employee_role', (err, res) => {
-            if (err) throw err;
-
+        if (err) {
+            console.error('Error fetching roles', err);
+            menu();
+        } else {
             const roles = res.rows.map(role => ({
                 name: role.title,
                 value: role.role_id
@@ -205,25 +195,84 @@ const updateEmployeeRole = () => {
 
             inquirer.prompt([
                 {
-                    type: 'list',
-                    name: 'employee',
-                    message: 'Select the employee to update',
-                    choices: employees
+                    type: 'input',
+                    name: 'first_name',
+                    message: 'Enter the first name of the employee'
+                },
+                {
+                    type: 'input',
+                    name: 'last_name',
+                    message: 'Enter the last name of the employee'
                 },
                 {
                     type: 'list',
                     name: 'role',
-                    message: 'Select the new role',
+                    message: 'Select the role of the employee',
                     choices: roles
                 }
-            ]).then(({employee, role}) => {
-                pool.query('UPDATE employee SET role_id = $1 WHERE employee_id = $2', [role, employee], (err, res) => {
-                    if (err) throw err;
-                    console.log('Employee role updated');
+            ]).then(({ first_name, last_name, role }) => {
+                pool.query('INSERT INTO employee (first_name, last_name, role_id) VALUES ($1, $2, $3)', [first_name, last_name, role], (err, res) => {
+                    if (err) {
+                        console.error('Error adding employee', err);
+                    } else {
+                        console.log('Employee added');
+                    }
                     menu();
                 });
             });
-        });
+        }
+    });
+};
+
+
+// Update an employee role
+const updateEmployeeRole = () => {
+    pool.query('SELECT * FROM employee', (err, res) => {
+        if (err) {
+            console.error('Error fetching employees', err);
+            menu();
+        } else {
+            const employees = res.rows.map(employee => ({
+                name: `${employee.first_name} ${employee.last_name}`,
+                value: employee.employee_id
+            }));
+
+            pool.query('SELECT * FROM employee_role', (err, res) => {
+                if (err) {
+                    console.error('Error fetching roles', err);
+                    menu();
+                } else {
+                    const roles = res.rows.map(role => ({
+                        name: role.title,
+                        value: role.role_id
+                    }));
+
+                    inquirer.prompt([
+                        {
+                            type: 'list',
+                            name: 'employee',
+                            message: 'Select the employee to update',
+                            choices: employees
+                        },
+                        {
+                            type: 'list',
+                            name: 'role',
+                            message: 'Select the new role',
+                            choices: roles
+                        }
+                    ]).then(({ employee, role }) => {
+                        pool.query('UPDATE employee SET role_id = $1 WHERE employee_id = $2', [role, employee], (err, res) => {
+                            if (err) {
+                                console.error('Error updating employee role', err);
+                            } else {
+                                console.log('Employee role updated');
+                            }
+                            menu();
+                        });
+                    });
+                }
+            });
+        }
     });
 };
 
